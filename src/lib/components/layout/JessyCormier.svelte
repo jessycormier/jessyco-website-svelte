e2c0adbb8e002e88be32b3fd97bc8d383545b125
Jessy Cormier <jessycormier@gmail.com>
2023-01-09 11:52:44 -0400

    adjust header and post responsiveness

--- src/lib/components/layout/JessyCormier.svelte
+++ src/lib/components/layout/JessyCormier.svelte
@@ -1,6 +1,6 @@
 <span class="dark:text-gray-50 text-black group font-mono text-center">
 	<span class="group-hover:opacity-100 opacity-70 transition-opacity duration-700"
 		>Jessy<span class="group-hover:opacity-0 opacity-40 transition-opacity duration-700">.</span
-		>Co<span class="group-hover:opacity-100 opacity-60 transition-opacity duration-500">rmier</span>
+		>Co<span class="group-hover:opacity-100 opacity-60 transition-opacity duration-500 hidden sm:inline">rmier</span>
 	</span>
 </span>
