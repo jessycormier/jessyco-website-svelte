365e7264b635fb99885a583e8981d25e25f23822
Jessy Cormier <jessycormier@gmail.com>
2023-01-29 21:45:18 -0400

    tweak how pages get rendered.

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,9 +1,9 @@
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const prerender = false;
-export const csr = false;
-export const ssr = true;
+export const prerender = 'auto';
+export const csr = true;
+// export const ssr = true;
 
 export const load = (async ({ url }) => {
 	try {
